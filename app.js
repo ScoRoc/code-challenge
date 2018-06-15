20e28a14b16e3bf51d24aeee5af4ff331880e3a1
stevens1434 <stevens1434@gmail.com>
2018-06-14 17:04:09 -0700

    heroku push

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var bodyParser = require('body-parser');
 
 //FOR MONGOOSE
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/boilerplate', { useMongoClient: true }); //commented out for heroku // "boilerplate" will be name of db
-// mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});  // for heroku deployment
+// mongoose.connect('mongodb://localhost/boilerplate', { useMongoClient: true }); //commented out for heroku // "boilerplate" will be name of db
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});  // for heroku deployment
 
 //FOR ROUTES
 var index = require('./routes/index');
@@ -22,8 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); //commented out for heroku
-// app.use(express.static(path.resolve(__dirname, 'client', 'build'))); //for heroku deployment
+// app.use(express.static(path.join(__dirname, 'public'))); //commented out for heroku
+app.use(express.static(path.resolve(__dirname, 'client', 'build'))); //for heroku deployment
 
 app.use(function(req, res, next) {
   // before every route, attach the flash messages and current user to res.locals
@@ -32,11 +32,11 @@ app.use(function(req, res, next) {
 });
 
 //FOR ROUTES AGAIN
-app.use('/', index);
+// app.use('/', index);
 // for heroku deployment
-// app.get('*', function(req, res, next) {
-// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('*', function(req, res, next) {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 ////PACKAGE.JSON ITEM: "heroku-postbuild": "cd client && npm install --only=dev && npm install && npm run build"
 ////PACKAGE.JSON ITEM: PORT=3001 - for non-heroku build
 
